2589cd1f13d03853a0bda68b0e857f835a3abf28
Pavan Javali <pavan.javali@globaldelight.com>
2025-07-11 12:45:16 +0530

    Add TabSwitcherView with delegate and animated indicator

--- TapAssignment/UI Components/TabSwitcherView.swift
+++ TapAssignment/UI Components/TabSwitcherView.swift
@@ -14,61 +14,77 @@ protocol TabSwitcherDelegate: AnyObject {
 }
 
 final class TabSwitcherView: UIView {
+
+    weak var delegate: TabSwitcherDelegate?
+
+    private let stackView = UIStackView()
+    private let indicatorView = UIView()
+
     private let titles = ["ISIN Analysis", "Pros & Cons"]
     private var buttons: [UIButton] = []
-    private let indicator = UIView()
-    weak var delegate: TabSwitcherDelegate?
+    private var selectedIndex: Int = 0
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupTabs()
+        setupView()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private func setupTabs() {
-        backgroundColor = .secondarySystemBackground
-        let stack = UIStackView()
-        stack.axis = .horizontal
-        stack.distribution = .fillEqually
-        stack.translatesAutoresizingMaskIntoConstraints = false
+    private func setupView() {
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(stackView)
 
-        for (i, title) in titles.enumerated() {
+        indicatorView.backgroundColor = .label
+        indicatorView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(indicatorView)
+
+        for (index, title) in titles.enumerated() {
             let button = UIButton(type: .system)
             button.setTitle(title, for: .normal)
-            button.setTitleColor(.label, for: .normal)
-            button.tag = i
+            button.titleLabel?.font = .systemFont(ofSize: 14, weight: .medium)
+            button.tag = index
             button.addTarget(self, action: #selector(tabTapped(_:)), for: .touchUpInside)
             buttons.append(button)
-            stack.addArrangedSubview(button)
+            stackView.addArrangedSubview(button)
         }
 
-        addSubview(stack)
-        addSubview(indicator)
-
-        indicator.backgroundColor = .systemBlue
-        indicator.translatesAutoresizingMaskIntoConstraints = false
-
         NSLayoutConstraint.activate([
-            stack.topAnchor.constraint(equalTo: topAnchor),
-            stack.leadingAnchor.constraint(equalTo: leadingAnchor),
-            stack.trailingAnchor.constraint(equalTo: trailingAnchor),
-            stack.bottomAnchor.constraint(equalTo: bottomAnchor),
-            indicator.bottomAnchor.constraint(equalTo: bottomAnchor),
-            indicator.heightAnchor.constraint(equalToConstant: 2),
-            indicator.leadingAnchor.constraint(equalTo: leadingAnchor),
-            indicator.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.5)
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stackView.heightAnchor.constraint(equalToConstant: 40),
+
+            indicatorView.heightAnchor.constraint(equalToConstant: 2),
+            indicatorView.bottomAnchor.constraint(equalTo: stackView.bottomAnchor),
+            indicatorView.widthAnchor.constraint(equalTo: stackView.widthAnchor, multiplier: 0.5),
+            indicatorView.leadingAnchor.constraint(equalTo: leadingAnchor)
         ])
     }
 
     @objc private func tabTapped(_ sender: UIButton) {
         let index = sender.tag
-        UIView.animate(withDuration: 0.25) {
-            self.indicator.frame.origin.x = CGFloat(index) * (self.frame.width / 2)
-        }
+        guard index != selectedIndex else { return }
+        selectedIndex = index
+        updateIndicator(animated: true)
         delegate?.didSwitchToTab(index: index)
     }
-}
 
+    private func updateIndicator(animated: Bool) {
+        let newLeading = CGFloat(selectedIndex) / CGFloat(titles.count)
+        let indicatorX = bounds.width * newLeading
+
+        UIView.animate(withDuration: animated ? 0.25 : 0.0) {
+            self.indicatorView.frame.origin.x = indicatorX
+        }
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        updateIndicator(animated: false)
+    }
+}
